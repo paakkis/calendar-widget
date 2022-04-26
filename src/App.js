a8f51549780b33873b753faf54e0683f8e9993be
paakkis <juusop2k12@gmail.com>
2022-04-26 17:38:45 +0300

    viilaukset

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import './App.css'
 import EventMapper from './components/EventMapper'
@@ -29,35 +28,3 @@ function App() {
 }
 
 export default App
-=======
-import React from 'react'
-import './App.css'
-import EventMapper from './components/EventMapper'
-import { useFetch } from './hooks/useFetch'
-
-function App() {
-  const [events, loading] = useFetch('https://skripti.org/calendarApi')
-
-  return (
-    <div className="App">
-      {loading ? (
-        <em>Ladataan :-)</em>
-      ) : (
-        <div>
-          <EventMapper events={events} />
-          <div className="App-info">
-            <a
-              className="App-link"
-              href="https://github.com/skriptiry/calendar-widget"
-            >
-              tapahtumakalenteri v. 0.2
-            </a>
-          </div>
-        </div>
-      )}
-    </div>
-  )
-}
-
-export default App
->>>>>>> f1820db1b55bc0ae1ae79f1c7492e85bbc1a92c4
